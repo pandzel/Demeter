7621c1fb75e1d8ab6e4c0432e8279f289c8b1806
pandzel <pandzel@esri.com>
2019-04-18 12:58:42 -0700

    WellKnownNamespaces class.

---
+++ demeter-service/src/main/java/com/panforge/demeter/service/WellKnownNamespaces.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 Piotr Andzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.panforge.demeter.service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+
+/**
+ * Well known names paces.
+ */
+public class WellKnownNamespaces {
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  /**
+   * Loads namespaces from 'well-known-namespaces.json'
+   */
+  public void load() {
+    try (InputStream inp = Thread.currentThread().getContextClassLoader().getResourceAsStream("well-known-namespaces.json");) {
+      Namespaces namespaces = mapper.readValue(inp, Namespaces.class);
+    } catch (Exception e) {}
+  }
+}
