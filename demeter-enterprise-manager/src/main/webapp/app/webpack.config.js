2b81c35b7bca3324e622a6744bd5bce909850777
pandzel <p.andzel@yahoo.com>
2019-05-20 20:55:11 -0700

    scss added.

--- demeter-enterprise-manager/src/main/webapp/app/webpack.config.js
+++ demeter-enterprise-manager/src/main/webapp/app/webpack.config.js
@@ -15,8 +15,12 @@ module.exports = {
             loader: 'babel-loader'
           },
           {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ]
           }
       ]
     },
