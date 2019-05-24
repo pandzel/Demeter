03ed9bec1c914b9ec19ca9e137a8df66400c1644
pandzel <p.andzel@yahoo.com>
2019-05-23 21:27:00 -0700

    ListIdentifiers for Cassandra

--- demeter-enterprise-server/src/main/java/com/panforge/demeter/server/beans/ContentProviderBean.java
+++ demeter-enterprise-server/src/main/java/com/panforge/demeter/server/beans/ContentProviderBean.java
@@ -27,26 +27,21 @@ import com.panforge.demeter.core.model.response.elements.Header;
 import com.panforge.demeter.core.model.response.elements.MetadataFormat;
 import com.panforge.demeter.core.model.response.elements.Record;
 import com.panforge.demeter.core.model.response.elements.Set;
-import com.panforge.demeter.core.utils.XmlUtils;
 import java.net.URI;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import com.panforge.demeter.core.content.Cursor;
-import com.panforge.demeter.server.MetaDescriptor;
 import com.panforge.demeter.server.MetaProcessorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 import com.panforge.demeter.core.content.ContentProvider;
 import com.panforge.demeter.core.content.Filter;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.StreamSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,108 +51,87 @@ import org.slf4j.LoggerFactory;
  */
 @Service
 public class ContentProviderBean implements ContentProvider {
+
   private static final Logger LOG = LoggerFactory.getLogger(ContentProviderBean.class);
-  
+
   @Autowired
   private MetaProcessorService metadataProcessorService;
-  
-  private final Map<URI, Map<String,MetaDescriptor>> descriptors = Collections.synchronizedMap(new HashMap<>());
-  
+
+  private CqlSession session;
+
   @PostConstruct
   public void construct() {
+    session = CqlSession.builder().withKeyspace(CqlIdentifier.fromCql("demeter")).build();
     LOG.info(String.format("%s created.", this.getClass().getSimpleName()));
   }
-  
+
   @PreDestroy
   public void destroy() {
+    if (session != null) {
+      session.close();
+    }
     LOG.info(String.format("%s destroyed.", this.getClass().getSimpleName()));
   }
 
   @Override
-  public Cursor<MetadataFormat> listMetadataFormats(URI uri) throws  IdDoesNotExistException, NoMetadataFormatsException {
+  public Cursor<MetadataFormat> listMetadataFormats(URI uri) throws IdDoesNotExistException, NoMetadataFormatsException {
     List<MetadataFormat> formats = metadataProcessorService.listMetadataFormats().stream().collect(Collectors.toList());
     if (formats.isEmpty()) {
-      throw new NoMetadataFormatsException(uri==null? String.format("No metadata formats"): String.format("No metadata formats for: %s", uri));
+      throw new NoMetadataFormatsException(uri == null ? String.format("No metadata formats") : String.format("No metadata formats for: %s", uri));
     }
     return Cursor.of(formats);
   }
 
   @Override
   public Cursor<Set> listSets() throws NoSetHierarchyException {
-    try (CqlSession session = CqlSession.builder().withKeyspace(CqlIdentifier.fromCql("demeter")).build()) {
-      ResultSet rs = session.execute("select * from sets");
-      int total = rs.getAvailableWithoutFetching();
-      return Cursor.of(StreamSupport.stream(rs.spliterator(), false).map(row -> { 
-        String setSpec = row.get("setSpec", String.class); 
-        String setName = row.get("setName", String.class); 
-        return new Set(setSpec, setName, null); 
-      }), total);
-    }
+    ResultSet rs = session.execute("select * from sets");
+    int total = rs.getAvailableWithoutFetching();
+    return Cursor.of(StreamSupport.stream(rs.spliterator(), false).map(row -> {
+      String setSpec = row.getString("setSpec");
+      String setName = row.getString("setName");
+      return new Set(setSpec, setName, null);
+    }), total);
+  }
+
+  private Cursor<UUID> listSetsIdsFor(String recordId) {
+    ResultSet rs = session.execute("select setId from records_sets where recordId = " + recordId);
+    int total = rs.getAvailableWithoutFetching();
+    return Cursor.of(StreamSupport.stream(rs.spliterator(), false).map(row -> row.getUuid("setId")), total);
+  }
+
+  private Cursor<Set> listSetsFor(String recordId) {
+    ResultSet rs = session.execute("select * from sets where id in (" + listSetsIdsFor(recordId).createStream().map(UUID::toString).collect(Collectors.joining(",")) + ")");
+    int total = rs.getAvailableWithoutFetching();
+    return Cursor.of(StreamSupport.stream(rs.spliterator(), false).map(row -> {
+      String setSpec = row.getString("setSpec");
+      String setName = row.getString("setName");
+      return new Set(setSpec, setName, null);
+    }), total);
   }
 
   @Override
   public Cursor<Header> listHeaders(Filter filter) throws CannotDisseminateFormatException, NoRecordsMatchException, NoSetHierarchyException {
-    if (filter.set!=null) {
-      throw new NoSetHierarchyException("This repository does not support set hierarchy.");
-    }
     try {
-      if (!StreamSupport.stream(listMetadataFormats(null).spliterator(), false).map(f->f.metadataPrefix).anyMatch(p->p.equals(filter.metadataPrefix))) {
+      if (!StreamSupport.stream(listMetadataFormats(null).spliterator(), false).map(f -> f.metadataPrefix).anyMatch(p -> p.equals(filter.metadataPrefix))) {
         throw new CannotDisseminateFormatException(String.format("Invalid metadata format prefix: '%s'", filter.metadataPrefix));
       }
-    } catch (NoMetadataFormatsException|IdDoesNotExistException ex) {
-        throw new CannotDisseminateFormatException(String.format("Invalid metadata format prefix: '%s'", filter.metadataPrefix), ex);
+    } catch (NoMetadataFormatsException | IdDoesNotExistException ex) {
+      throw new CannotDisseminateFormatException(String.format("Invalid metadata format prefix: '%s'", filter.metadataPrefix), ex);
     }
-    List<Header> headers = descriptors.values().stream()
-                    .map(l -> {
-                      MetaDescriptor md = l.values().stream()
-                              .filter(desc->desc.matches(filter))
-                              .sorted((a, b) -> a.datestamp.compareTo(b.datestamp))
-                              .findFirst()
-                              .orElse(null);
-                      return md != null ? md.toHeader() : null;
-                    })
-                    .filter(h -> h != null)
-                    .collect(Collectors.toList());    
-    
-    if (headers.isEmpty()) {
-      throw new NoRecordsMatchException(String.format("No matching records."));
-    }
-    return Cursor.of(headers);
+    ResultSet rs = session.execute("select id, identifier, date from records");
+    int total = rs.getAvailableWithoutFetching();
+    return Cursor.of(StreamSupport.stream(rs.spliterator(), false).map(row -> {
+      UUID id = row.getUuid("id");
+      String identifier = row.getString("identifier");
+      LocalDate localDate = row.getLocalDate("date");
+      List<String> sets = listSetsFor(id.toString()).createStream().map(s->s.setSpec).collect(Collectors.toList());
+      String [] setsArray = !sets.isEmpty()? sets.toArray(new String[sets.size()]): null;
+      return new Header(URI.create(identifier), OffsetDateTime.ofInstant(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant(), ZoneId.systemDefault()), setsArray, false);
+    }), total);
   }
 
   @Override
   public Record readRecord(URI identifier, String metadataPrefix) throws IdDoesNotExistException, CannotDisseminateFormatException {
-    Map<String, MetaDescriptor> commonDescriptors = descriptors.get(identifier);
-    if (commonDescriptors==null) {
-      throw new IdDoesNotExistException(identifier.toASCIIString());
-    }
-    MetaDescriptor md = commonDescriptors.get(metadataPrefix);
-    if (md==null) {
-      throw new CannotDisseminateFormatException(metadataPrefix);
-    }
-    Header header = md.toHeader();
-    Document doc = parseToXml(md.source);
-    if (doc!=null) {
-      doc = md.mp.adopt(md.source, doc);
-    }
-    
-    return new Record(header, doc, null);
-  }
-  
-  private void storeDescriptor(MetaDescriptor md) {
-    Map<String, MetaDescriptor> commonDescriptors = descriptors.get(md.uri);
-    if (commonDescriptors==null) {
-      commonDescriptors = Collections.synchronizedMap(new HashMap<>());
-      descriptors.put(md.uri, commonDescriptors);
-    }
-    commonDescriptors.put(md.format.metadataPrefix, md);
-  }
-  
-  private Document parseToXml(File file) {
-    try {
-      return XmlUtils.parseToXml(file);
-    } catch (IOException | SAXException ex) {
-      return null;
-    }
+    throw new IdDoesNotExistException(identifier.toString());
   }
 }
