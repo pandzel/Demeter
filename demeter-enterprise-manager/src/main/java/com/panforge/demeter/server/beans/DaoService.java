ba50a18252813ea281accf022cc241be8d8d578a
pandzel <p.andzel@yahoo.com>
2019-05-28 21:32:36 -0700

    SetController added.

---
+++ demeter-enterprise-manager/src/main/java/com/panforge/demeter/server/beans/DaoService.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2019 Piotr Andzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.panforge.demeter.server.beans;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.panforge.demeter.server.Dao;
+import com.panforge.demeter.server.elements.SetData;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * DAO service.
+ */
+@Service
+public class DaoService implements Dao {
+  private final Logger LOG = LoggerFactory.getLogger(DaoService.class);
+
+  private CqlSession session;
+  
+  @PostConstruct
+  public void construct() {
+    session = CqlSession.builder().withKeyspace(CqlIdentifier.fromCql("demeter")).build();
+    LOG.info(String.format("%s created.", this.getClass().getSimpleName()));
+  }
+  
+  @PreDestroy
+  public void destroy() {
+    if (session != null) {
+      session.close();
+    }
+    LOG.info(String.format("%s destroyed.", this.getClass().getSimpleName()));
+  }
+  
+  @Override
+  public CqlSession getSession() {
+    return session;
+  }
+  
+  public List<SetData> listSets() {
+    ResultSet rs = session.execute("select * from sets");
+    return rs.all().stream()
+            .map(row -> {
+              SetData setData = new SetData();
+              
+              setData.id = row.getUuid("id");
+              setData.setSpec = row.getString("setSpec");
+              setData.setName = row.getString("setName");
+              
+              return setData;
+            })
+            .collect(Collectors.toList());
+  }
+}
---
+++ demeter-enterprise-manager/src/main/java/com/panforge/demeter/server/rest/SetController.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Piotr Andzel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.panforge.demeter.server.rest;
+
+import com.panforge.demeter.server.Dao;
+import com.panforge.demeter.server.elements.SetData;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * OAI controller
+ */
+@RestController
+@RequestMapping("/rest")
+public class SetController {
+  private final Logger LOG = LoggerFactory.getLogger(SetController.class);
+  
+  private final Dao dao;
+  
+  @Autowired 
+  public SetController(Dao dao) {
+    this.dao = dao;
+  }
+  
+  @PostConstruct
+  public void construct() {
+    LOG.info(String.format("%s created.", this.getClass().getSimpleName()));
+  }
+  
+  @PreDestroy
+  public void destroy() {
+    LOG.info(String.format("%s destroyed.", this.getClass().getSimpleName()));
+  }
+  
+  @RequestMapping(value = "/sets", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<SetData>> list(HttpServletRequest request) {
+    try {
+      LOG.debug(String.format("Received request '%s'", request.getQueryString()));
+      return new ResponseEntity<>(dao.listSets(), HttpStatus.OK);
+    } catch (Exception ex) {
+      LOG.error(String.format("Error processing request '%s'", request.getQueryString()), ex);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
