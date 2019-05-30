ca5455e3ce14ec3793e0396e0b79113d5a3f029a
pandzel <p.andzel@yahoo.com>
2019-05-30 16:42:23 -0700

    Icons

--- demeter-enterprise-manager/src/main/webapp/app/src/layout/Navi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/layout/Navi.js
@@ -1,5 +1,10 @@
 import React, { Component} from "react";
 import "./Navi.scss";
+import iconHome from '../images/icon-home.png';
+import iconSets from '../images/icon-sets.png';
+import iconFiles from '../images/icon-files.png';
+import iconTools from '../images/icon-tools.png';
+import iconSettings from '../images/icon-settings.png';
 
 export default
 class Navi extends Component{
@@ -11,14 +16,17 @@ class Navi extends Component{
   };
   
   handleClick(e) {
-    e.preventDefault();
-    this.props.onNaviClick({choice: 1});
+    this.props.onNaviClick({choice: e});
   };
   
   render(){
     return(
-      <div className="Navi" onClick={this.handleClick}>
-        <div></div>
+      <div className="Navi">
+        <img src={iconHome} width="80" height="80" title="Home" onClick={()=>this.handleClick('home')} />
+        <img src={iconSets} width="80" height="80" title="Sets" onClick={()=>this.handleClick('sets')} />
+        <img src={iconFiles} width="80" height="80" title="Data" onClick={()=>this.handleClick('files')} />
+        <img src={iconTools} width="80" height="80" title="Tools" onClick={()=>this.handleClick('tools')} />
+        <img src={iconSettings} width="80" height="80" title="Settings" onClick={()=>this.handleClick('settings')}/>
       </div>
     );
   };
