a433e2025b5155a6854fce212e1ca0ec68c6330e
pandzel <p.andzel@yahoo.com>
2019-05-31 06:30:31 -0700

    Refactoring folder structure for ReactJS.

--- demeter-enterprise-manager/src/main/webapp/app/src/index.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App.js";
+import App from "./main/App.js";
 
 ReactDOM.render(
     <App/>,
--- demeter-enterprise-manager/src/main/webapp/app/src/main/App.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/main/App.js
@@ -1,9 +1,9 @@
 import React, { Component} from "react";
 import "./App.scss";
 
-import Header from "./layout/Header";
-import Stage  from "./layout/Stage";
-import Footer from "./layout/Footer";
+import Header from "../layout/Header";
+import Stage  from "../layout/Stage";
+import Footer from "../layout/Footer";
 
 export default
 class App extends Component{
