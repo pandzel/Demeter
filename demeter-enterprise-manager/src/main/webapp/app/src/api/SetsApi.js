d6111852496f46d89bd3fa9258866ec1c29acb7b
pandzel <pandzel@esri.com>
2019-05-31 11:27:29 -0700

    Axios and reading sets from REST.

--- demeter-enterprise-manager/src/main/webapp/app/src/api/SetsApi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/api/SetsApi.js
@@ -1,14 +1,10 @@
+import axios from 'axios';
+
 export default
 class SetsApi {
   list() {
-    var allSets = [];
-    allSets.push({
-      id: '0000000',
-      setSpec: 'sample',
-      setName: 'Sample set'
-    });
     return new Promise((resolve, reject) => {
-      resolve(allSets);
+      axios.get('rest/sets').then((result)=>resolve(result.data));
     });
   }
 }
