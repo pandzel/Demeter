f9691e83368965b059193b9122d5bb742edf6172
pandzel <pandzel@esri.com>
2019-06-03 08:22:49 -0700

    SetsTable table events.

--- demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsTable.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsTable.js
@@ -10,13 +10,12 @@ class SetsTable extends Component{
   constructor(props) {
     super(props);
     this.state  = { data: this.props.data };
-    this.setNameEditor = this.setNameEditor.bind(this);
-    this.setSpecEditor = this.setSpecEditor.bind(this);
   }
   
   actionTemplate(rowData, column) {
     return (<div>
-              <Button type="button" icon="pi pi-trash" className="p-button-warning"></Button>
+              <Button type="button" icon="pi pi-trash" className="p-button-warning" onClick={() => this.onDelete(rowData)}></Button>
+              <Button type="button" icon="pi pi-folder-open" className="p-button-info" onClick={() => this.onInfo(rowData)}></Button>
             </div>);
   }
     
@@ -25,22 +24,39 @@ class SetsTable extends Component{
       updatedItems[props.rowIndex][props.field] = value;
       this.setState({data: updatedItems});
   }
+  
+  onInfo(props) {
+    // display info
+    console.log("More Info", props);
+  }
+  
+  onDelete(props) {
+    // save state
+    console.log("Delete", props);
+  }
+  
+  onUpdate(props) {
+    // save state
+    console.log("Update", props);
+  }
 
-  setNameEditor(props) {
-      return <InputText type="text" value={this.state.data[props.rowIndex]['setName']} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+  nameEditor(props) {
+      return <InputText type="text" value={this.state.data[props.rowIndex]['setName']} 
+              onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
   }    
 
-  setSpecEditor(props) {
-      return <InputText type="text" value={this.state.data[props.rowIndex]['setSpec']} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+  specEditor(props) {
+      return <InputText type="text" value={this.state.data[props.rowIndex]['setSpec']} 
+              onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
   }    
   
   render(){
     return(
       <div className="SetsTable">
         <DataTable value={this.state.data}>
-          <Column field="setName" header="Name" editor={this.setNameEditor}/>
-          <Column field="setSpec" header="Spec" editor={this.setSpecEditor}/>
-          <Column body={this.actionTemplate} style={{textAlign:'center', width: '6em'}}/>
+          <Column field="setName" header="Name" editor={props => this.nameEditor(props)} onEditorSubmit={props => this.onUpdate(props)} />
+          <Column field="setSpec" header="Spec" editor={props => this.specEditor(props)}/>
+          <Column body={(rowData, column) => this.actionTemplate(rowData, column)} style={{textAlign:'center', width: '10em'}}/>
         </DataTable>
       </div>
     );
