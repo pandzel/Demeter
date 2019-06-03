7c30e7430517c223f6fe4cb9a0b85326df4166d2
pandzel <p.andzel@yahoo.com>
2019-06-02 21:43:31 -0700

    Action row.

--- demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsTable.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsTable.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import "./SetsPane.scss";
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
+import {Button} from 'primereact/button';
 
 export default
 class SetsTable extends Component{
@@ -10,12 +11,19 @@ class SetsTable extends Component{
     this.state  = { data: this.props.data };
   }
   
+  actionTemplate(rowData, column) {
+    return (<div>
+              <Button type="button" icon="pi pi-trash" className="p-button-warning"></Button>
+            </div>);
+  }
+    
   render(){
     return(
       <div className="SetsTable">
         <DataTable value={this.state.data}>
           <Column field="setName" header="Name"/>
           <Column field="setSpec" header="Spec"/>
+          <Column body={this.actionTemplate} style={{textAlign:'center', width: '6em'}}/>
         </DataTable>
       </div>
     );
