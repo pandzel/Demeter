c384922e2b4457b40b704b262f16ee51b0360d3e
pandzel <p.andzel@yahoo.com>
2019-06-02 21:52:12 -0700

    Set name and spec editor.

--- demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsTable.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsTable.js
@@ -3,12 +3,15 @@ import "./SetsPane.scss";
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
 import {Button} from 'primereact/button';
+import {InputText} from 'primereact/inputtext';
 
 export default
 class SetsTable extends Component{
   constructor(props) {
     super(props);
     this.state  = { data: this.props.data };
+    this.setNameEditor = this.setNameEditor.bind(this);
+    this.setSpecEditor = this.setSpecEditor.bind(this);
   }
   
   actionTemplate(rowData, column) {
@@ -17,12 +20,26 @@ class SetsTable extends Component{
             </div>);
   }
     
+  onEditorValueChange(props, value) {
+      let updatedItems = [...this.state.data];
+      updatedItems[props.rowIndex][props.field] = value;
+      this.setState({data: updatedItems});
+  }
+
+  setNameEditor(props) {
+      return <InputText type="text" value={this.state.data[props.rowIndex]['setName']} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+  }    
+
+  setSpecEditor(props) {
+      return <InputText type="text" value={this.state.data[props.rowIndex]['setSpec']} onChange={(e) => this.onEditorValueChange(props, e.target.value)} />;
+  }    
+  
   render(){
     return(
       <div className="SetsTable">
         <DataTable value={this.state.data}>
-          <Column field="setName" header="Name"/>
-          <Column field="setSpec" header="Spec"/>
+          <Column field="setName" header="Name" editor={this.setNameEditor}/>
+          <Column field="setSpec" header="Spec" editor={this.setSpecEditor}/>
           <Column body={this.actionTemplate} style={{textAlign:'center', width: '6em'}}/>
         </DataTable>
       </div>
