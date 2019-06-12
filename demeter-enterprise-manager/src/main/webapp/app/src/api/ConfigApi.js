a7460aefc839faf199990fcba5e802165f24e568
pandzel <p.andzel@yahoo.com>
2019-06-11 19:15:56 -0700

    SettingsForm.

--- demeter-enterprise-manager/src/main/webapp/app/src/api/ConfigApi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/api/ConfigApi.js
@@ -7,4 +7,11 @@ class ConfigApi {
       axios.get(`${config.context}rest/config`).then((result)=>resolve(result.data));
     });
   }
+  
+  save(data) {
+    return new Promise((resolve, reject) => {
+      // TODO: provide implementation once server side is done
+      resolve({success: true});
+    });
+  }
 }
--- demeter-enterprise-manager/src/main/webapp/app/src/settings/SettingsPane.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/settings/SettingsPane.js
@@ -1,15 +1,16 @@
 import React, { Component} from "react";
 import "./SettingsPane.scss";
 import ConfigApi from '../api/ConfigApi';
-import {InputText} from 'primereact/inputtext';
+import SettingsForm from './SettingsForm';
 
 export default
 class SettingsPane extends Component{
   state = {};
   
+  api = new ConfigApi();
+  
   componentDidMount() {
-    const api = new ConfigApi();
-    api.load().then(result => {
+    this.api.load().then(result => {
       result.adminEmail = result.adminEmail? result.adminEmail.join(', '): '';
       this.setState({
         data: result
@@ -21,17 +22,7 @@ class SettingsPane extends Component{
     return(
       <div className="SettingsPane">
         <div className="Title">Settings</div>
-        <div className="SettingsForm">
-          <div>
-            <span>Repository name:</span><span><InputText value={this.state.repositoryName} placeholder='Enter repository name'/></span>
-          </div>
-          <div>
-            <span>Base URL:</span><span><InputText value={this.state.baseURL} placeholder='Enter repository base URL'/></span>
-          </div>
-          <div>
-            <span>Administrators emails:</span><span><InputText value={this.state.adminEmail} placeholder='List administrators email(s)'/></span>
-          </div>
-        </div>
+        {this.state.data && <SettingsForm data={this.state.data}/>}
       </div>
     );
   }
