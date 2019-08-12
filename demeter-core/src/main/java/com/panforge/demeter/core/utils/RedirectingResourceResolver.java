a620f5adca4166e82c670f5c64e1fed124eb70d5
pandzel <p.andzel@yahoo.com>
2019-08-11 20:52:59 -0700

    RedirectingResourceResolver added.

--- demeter-core/src/main/java/com/panforge/demeter/core/utils/RedirectingResourceResolver.java
+++ demeter-core/src/main/java/com/panforge/demeter/core/utils/RedirectingResourceResolver.java
@@ -15,38 +15,133 @@
  */
 package com.panforge.demeter.core.utils;
 
-import com.sun.org.apache.xerces.internal.dom.DOMInputImpl;
-import com.sun.org.apache.xerces.internal.util.XMLCatalogResolver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
 
 /**
  * Redirecting resource resolver.
  */
-public class RedirectingResourceResolver extends XMLCatalogResolver {
-      @Override
-      public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
-        if (systemId!=null) {
-          try {
-            URL url = new URL(systemId);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            if (connection.getResponseCode()==301 || connection.getResponseCode()==302) {
-              String location = connection.getHeaderField("Location");
-              if (location!=null && !location.isEmpty()) {
-                url = new URL(location);
-                connection = (HttpURLConnection) url.openConnection();
-                InputStream stream = connection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
-                return new DOMInputImpl(null, null, null, reader, "UTF-8");
-              }
-            }
-          } catch (IOException ex) {}
+public class RedirectingResourceResolver implements LSResourceResolver {
+
+  @Override
+  public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
+    if (systemId != null) {
+      try {
+        URL url = new URL(systemId);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        if (connection.getResponseCode() == 301 || connection.getResponseCode() == 302) {
+          String location = connection.getHeaderField("Location");
+          if (location != null && !location.isEmpty()) {
+            url = new URL(location);
+            connection = (HttpURLConnection) url.openConnection();
+            InputStream stream = connection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+            Input input = new Input();
+            input.setCharacterStream(reader);
+            return input;
+          }
         }
-        return super.resolveResource(type, namespaceURI, publicId, systemId, baseURI); //To change body of generated methods, choose Tools | Templates.
+      } catch (IOException ex) {
       }
+    }
+    return null;
+  }
+
+  private final static class Input implements LSInput {
+    private Reader characterStream;
+    private InputStream byteStream;
+    private String stringData;
+    private String systemId;
+    private String publicId;
+    private String baseURI;
+    private String encoding;
+    private boolean certifiedText;
+
+    @Override
+    public Reader getCharacterStream() {
+      return characterStream;
+    }
+
+    @Override
+    public void setCharacterStream(Reader characterStream) {
+      this.characterStream = characterStream;
+    }
+
+    @Override
+    public InputStream getByteStream() {
+      return byteStream;
+    }
+
+    @Override
+    public void setByteStream(InputStream byteStream) {
+      this.byteStream = byteStream;
+    }
+
+    @Override
+    public String getStringData() {
+      return stringData;
+    }
+
+    @Override
+    public void setStringData(String stringData) {
+      this.stringData = stringData;
+    }
+
+    @Override
+    public String getSystemId() {
+      return systemId;
+    }
+
+    @Override
+    public void setSystemId(String systemId) {
+      this.systemId = systemId;
+    }
+
+    @Override
+    public String getPublicId() {
+      return publicId;
+    }
+
+    @Override
+    public void setPublicId(String publicId) {
+      this.publicId = publicId;
+    }
+
+    @Override
+    public String getBaseURI() {
+      return baseURI;
+    }
+
+    @Override
+    public void setBaseURI(String baseURI) {
+      this.baseURI = baseURI;
+    }
+
+    @Override
+    public String getEncoding() {
+      return encoding;
+    }
+
+    @Override
+    public void setEncoding(String encoding) {
+      this.encoding = encoding;
+    }
+
+    @Override
+    public boolean getCertifiedText() {
+      return certifiedText;
+    }
+
+    @Override
+    public void setCertifiedText(boolean certifiedText) {
+      this.certifiedText = certifiedText;
+    }
   
+  }
 }
