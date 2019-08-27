a803530812ff90905081ec1c45348236a0cb07cb
pandzel <p.andzel@yahoo.com>
2019-08-26 21:20:48 -0700

    Enterprise manager action icons for data.

--- demeter-enterprise-manager/src/main/webapp/app/src/data/DataTable.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/data/DataTable.js
@@ -15,13 +15,47 @@ class SetsTable extends Component{
   
   api = new RecordsApi();
   
+  actionTemplate = (rowData, column) => {
+    return (<div>
+              <Button type="button" icon="pi pi-trash" className="p-button-warning action-button delete-button" 
+                      title="Delete record"
+                      onClick={() => this.onDelete(rowData)}/>
+              <Button type="button" icon="pi pi-pencil" className="p-button-edit action-button edit-button" 
+                      title="Edit record"
+                      onClick={() => this.onEdit(rowData)}/>
+              <Button type="button" icon="pi pi-list" className="p-button-info action-button info-button" 
+                      title="Show more information"
+                      onClick={() => this.onInfo(rowData)}/>
+            </div>);
+  }
+  
+  onInfo = (props) => {
+    console.log("More info", props);
+  }
+  
+  onEdit = (props) => {
+    console.log("Editing data", props);
+  }
+  
+  onDelete = (props) => {
+    console.log("Deleting data", props);
+  }
+  
+  onAdd = () => {
+    console.log("Adding new record");
+  }
+  
   render(){
     return(
       <div className="DataTable">
         <DataTable value={this.state.data}>
           <Column field="title" header="Title"/>
           <Column field="description" header="Description"/>
+          <Column body={(rowData, column) => this.actionTemplate(rowData, column)} className="action-buttons"/>
         </DataTable>
+        <Button type="button" icon="pi pi-plus" className="p-button-info add" 
+                title="Add new record"
+                onClick={this.onAdd}/>
       </div>
     );
   }
