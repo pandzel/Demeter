258ff7b34178a9a466c8e5845d8eea8acf2ac283
pandzel <pandzel@esri.com>
2019-08-28 12:05:32 -0700

    RecordsApi updated.

--- demeter-enterprise-manager/src/main/webapp/app/src/api/RecordsApi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/api/RecordsApi.js
@@ -7,4 +7,22 @@ class RecordsApi {
       axios.get(`${config.context}rest/records`).then((result)=>resolve(result.data.data));
     });
   }
+  
+  create(data) {
+    return new Promise((resolve, reject) => {
+      axios.post(`${config.context}rest/records`, data==undefined? {}: data).then((result)=>resolve(result.data));
+    });
+  }
+  
+  delete(id) {
+    return new Promise((resolve, reject) => {
+      axios.delete(`${config.context}rest/records/${id}`).then((result)=>resolve(result.data));
+    });
+  }
+  
+  update(data) {
+    return new Promise((resolve, reject) => {
+      axios.put(`${config.context}rest/records/${data.id}`, data).then((result)=>resolve(result.data));
+    });
+  }
 }
