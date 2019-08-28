d950964c9918bddbf5031d172d2464e8ce402e20
pandzel <pandzel@esri.com>
2019-08-28 12:17:09 -0700

    Better REST error handling.

--- demeter-enterprise-manager/src/main/webapp/app/src/api/RecordsApi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/api/RecordsApi.js
@@ -4,25 +4,33 @@ export default
 class RecordsApi {
   list() {
     return new Promise((resolve, reject) => {
-      axios.get(`${config.context}rest/records`).then((result)=>resolve(result.data.data));
+      axios.get(`${config.context}rest/records`)
+              .then((result)=>resolve(result.data.data))
+              .catch((error)=>reject(error));
     });
   }
   
   create(data) {
     return new Promise((resolve, reject) => {
-      axios.post(`${config.context}rest/records`, data==undefined? {}: data).then((result)=>resolve(result.data));
+      axios.post(`${config.context}rest/records`, data==undefined? {}: data)
+              .then((result)=>resolve(result.data))
+              .catch((error)=>reject(error));
     });
   }
   
   delete(id) {
     return new Promise((resolve, reject) => {
-      axios.delete(`${config.context}rest/records/${id}`).then((result)=>resolve(result.data));
+      axios.delete(`${config.context}rest/records/${id}`)
+              .then((result)=>resolve(result.data))
+              .catch((error)=>reject(error));
     });
   }
   
   update(data) {
     return new Promise((resolve, reject) => {
-      axios.put(`${config.context}rest/records/${data.id}`, data).then((result)=>resolve(result.data));
+      axios.put(`${config.context}rest/records/${data.id}`, data)
+              .then((result)=>resolve(result.data))
+              .catch((error)=>reject(error));
     });
   }
 }
--- demeter-enterprise-manager/src/main/webapp/app/src/api/SetsApi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/api/SetsApi.js
@@ -4,25 +4,33 @@ export default
 class SetsApi {
   list() {
     return new Promise((resolve, reject) => {
-      axios.get(`${config.context}rest/sets`).then((result)=>resolve(result.data.data));
+      axios.get(`${config.context}rest/sets`)
+              .then((result)=>resolve(result.data.data))
+              .catch((error)=>reject(error));
     });
   }
   
   create(data) {
     return new Promise((resolve, reject) => {
-      axios.post(`${config.context}rest/sets`, data==undefined? {}: data).then((result)=>resolve(result.data));
+      axios.post(`${config.context}rest/sets`, data==undefined? {}: data)
+              .then((result)=>resolve(result.data))
+              .catch((error)=>reject(error));
     });
   }
   
   delete(id) {
     return new Promise((resolve, reject) => {
-      axios.delete(`${config.context}rest/sets/${id}`).then((result)=>resolve(result.data));
+      axios.delete(`${config.context}rest/sets/${id}`)
+              .then((result)=>resolve(result.data))
+              .catch((error)=>reject(error));
     });
   }
   
   update(data) {
     return new Promise((resolve, reject) => {
-      axios.put(`${config.context}rest/sets/${data.id}`, data).then((result)=>resolve(result.data));
+      axios.put(`${config.context}rest/sets/${data.id}`, data)
+              .then((result)=>resolve(result.data))
+              .catch((error)=>reject(error));
     });
   }
 }
