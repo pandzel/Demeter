b98acdf9f1f52b0c80f267ffd24f632056914d90
pandzel <p.andzel@yahoo.com>
2019-09-02 13:41:28 -0700

    Cleanup.

--- demeter-enterprise-manager/src/main/webapp/app/src/layout/Content.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/layout/Content.js
@@ -1,10 +1,8 @@
 import React, { Component} from "react";
 import "./Content.scss";
 
-import HomePane     from '../home/HomePane';
 import SetsPane     from '../sets/SetsPane';
 import DataPane     from '../data/DataPane';
-import ToolsPane    from '../tools/ToolsPane';
 import SettingsPane from '../settings/SettingsPane';
 
 export default
@@ -13,17 +11,11 @@ class Content extends Component{
   render(){
     var contentPane;
     switch (this.props.content) {
-      case "home":
-        contentPane = <HomePane/>;
-        break;
       case "sets":
         contentPane = <SetsPane/>;
         break;
       case "data":
         contentPane = <DataPane/>;
-        break;
-      case "tools":
-        contentPane = <ToolsPane/>;
         break;
       case "settings":
         contentPane = <SettingsPane/>;
