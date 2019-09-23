106f72db67f5364fc760c8edc56ebce8bab9e5b5
pandzel <pandzel@esri.com>
2019-09-23 07:49:16 -0700

    ConfigApi logs.

--- demeter-enterprise-manager/src/main/webapp/app/src/api/ConfigApi.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/api/ConfigApi.js
@@ -4,13 +4,19 @@ export default
 class ConfigApi {
   load() {
     return new Promise((resolve, reject) => {
-      axios.get(`${config.context}rest/config`).then((result)=>resolve(result.data));
+      axios.get(`${config.context}rest/config`).then((result)=>{
+        console.log("ConfigApi load()", result);
+        resolve(result.data);
+      });
     });
   }
   
   save(data) {
     return new Promise((resolve, reject) => {
-      axios.post(`${config.context}rest/config`, data).then((result)=>resolve(result));
+      axios.post(`${config.context}rest/config`, data).then((result)=>{
+        console.log("ConfigApi save()", result);
+        resolve(result);
+      });
     });
   }
 }
