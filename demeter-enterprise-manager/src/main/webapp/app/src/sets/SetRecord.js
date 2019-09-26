6fe0238b29c51b5745aba24d146bf496e5b8b79c
pandzel <p.andzel@yahoo.com>
2019-09-25 19:31:17 -0700

    SetsTable complete.

--- demeter-enterprise-manager/src/main/webapp/app/src/sets/SetRecord.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/sets/SetRecord.js
@@ -5,7 +5,7 @@ import {Button} from 'primereact/button';
 export default
 class SetRecord extends Component{
   render(){
-    return(<div className="Record">
+    return(<div className="SetRecord">
             <span>
               <Button type="button" icon="pi pi-times-circle"
                       title="Remove"
