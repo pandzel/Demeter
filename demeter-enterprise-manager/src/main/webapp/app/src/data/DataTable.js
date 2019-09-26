2e38e330a134e14d718fe194ff68294400533e70
pandzel <pandzel@esri.com>
2019-09-26 10:52:36 -0700

    DataTable work.

--- demeter-enterprise-manager/src/main/webapp/app/src/data/DataTable.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/data/DataTable.js
@@ -2,8 +2,14 @@ import React, { Component} from "react";
 import "./Data.scss";
 import DataApi from '../api/RecordsApi';
 
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {Button} from 'primereact/button';
+import {InputText} from 'primereact/inputtext';
+import {Paginator} from 'primereact/paginator';
+
 export default
-class DataTable extends Component{
+class DataTableComponent extends Component{
   state = {
     data: {
       page: 0,
@@ -25,9 +31,52 @@ class DataTable extends Component{
     });
   }
   
+  actionTemplate = (rowData, column) => {
+    return (<div>
+              <Button type="button" icon="pi pi-trash" className="p-button-warning action-button delete-button" 
+                      title="Delete record"
+                      onClick={() => this.onDelete(rowData)}/>
+              <Button type="button" icon="pi pi-pencil" className="p-button-edit action-button edit-button" 
+                      title="Edit record"
+                      onClick={() => this.onEdit(rowData)}/>
+              <Button type="button" icon="pi pi-list" className="p-button-info action-button info-button" 
+                      title="Show more information"
+                      onClick={() => this.onInfo(rowData)}/>
+            </div>);
+  }
+  
+  onInfo = (props) => {
+    // this.props.onInfo(props);
+  }
+  
+  onEdit = (props) => {
+    // this.props.onEdit(props);
+  }
+  
+  onDelete = (props) => {
+    // this.props.onDelete(props);
+  }
+  
+  onAdd = (props) => {
+    
+  }
+  
+  onPageChange = (page) => {
+    
+  }
+  
   render(){
     return(
       <div className="DataTable">
+        <Paginator first={this.state.data.page * this.state.data.pageSize} rows={this.state.data.pageSize} totalRecords={this.state.data.total} onPageChange={(e) => this.onPageChange(e.page)}></Paginator>
+        <DataTable value={this.state.data.data}>
+          <Column field="title" header="Title"/>
+          <Column field="description" header="Description"/>
+          <Column body={(rowData, column) => this.actionTemplate(rowData, column)} className="action-buttons"/>
+        </DataTable>
+        <Button type="button" icon="pi pi-plus" className="p-button-info add" 
+                title="Add new record"
+                onClick={this.onAdd}/>
       </div>
     );
   }
