d3311bfe85480532d620b22cc08c0e003e31635d
pandzel <pandzel@esri.com>
2019-10-02 07:50:50 -0700

    Title suffix.

--- demeter-enterprise-manager/src/main/webapp/app/src/data/DataPane.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/data/DataPane.js
@@ -7,6 +7,7 @@ import EditorPane from "../common/EditorPane";
 export default
 class DataPane extends Component{
   state = {
+    title: "",
     currentData: null,
     currentPage: undefined,
     showSets: false
@@ -16,21 +17,21 @@ class DataPane extends Component{
   }
   
   onShowData = (page) => {
-    this.setState({currentData: null, currentPage: page, showSets: false});
+    this.setState({currentData: null, currentPage: page, showSets: false, title: ""});
   }
   
   onEditData = (data, page) => {
-    this.setState({currentData: data, currentPage: page, showSets: false});
+    this.setState({currentData: data, currentPage: page, showSets: false, title: data.title? `(${data.title})`: ""});
   }
   
   onShowSets = (data, page) => {
-    this.setState({currentData: data, currentPage: page, showSets: true});
+    this.setState({currentData: data, currentPage: page, showSets: true, title: data.title? `(${data.title})`: ""});
   }
   
   render(){
     return(
       <div className="DataPane">
-        <div className="Title">Data</div>
+        <div className="Title">Data <span className="TitleSuffix">{this.state.title}</span></div>
         {this.state.currentData===null && <DataTable page={this.state.currentPage} onEdit={this.onEditData} onShowSets={this.onShowSets} onError={this.props.onError}/>}
         {this.state.currentData!==null && !this.state.showSets && <EditorPane record={this.state.currentData} onExit={this.onShowData} onError={this.props.onError}/>}
         {this.state.currentData!==null && this.state.showSets && <RecordSets record={this.state.currentData} onExit={this.onShowData} onError={this.props.onError}/>}
--- demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsPane.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/sets/SetsPane.js
@@ -6,6 +6,7 @@ import SetRecords from "./SetRecords";
 export default
 class SetsPane extends Component{
   state = {
+    title: "",
     currentSet: null
   };
   
@@ -13,17 +14,17 @@ class SetsPane extends Component{
   }
   
   onShowRecords = (props) => {
-    this.setState({currentSet: props});
+    this.setState({currentSet: props, title: props.setName? `(${props.setName})`: ""});
   }
   
   onShowSets = (props) => {
-    this.setState({currentSet: null});
+    this.setState({currentSet: null, title: ""});
   }
   
   render(){
     return(
       <div className="SetsPane">
-        <div className="Title">Sets</div>
+        <div className="Title">Sets <span className="TitleSuffix">{this.state.title}</span></div>
         {this.state.currentSet===null && <SetsTable onShowRecords={this.onShowRecords} onError={this.props.onError}/>}
         {this.state.currentSet!==null && <SetRecords cols={2} currentSet={this.state.currentSet} onExit={this.onShowSets} onError={this.props.onError}/>}
       </div>
