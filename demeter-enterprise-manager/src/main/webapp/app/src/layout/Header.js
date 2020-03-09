d9bb3c52f81f69d7d8fc5f1d53bb06ce5aa969b5
pandzel <p.andzel@yahoo.com>
2020-03-09 06:08:53 -0700

    2.1.1  Snapshot

--- demeter-enterprise-manager/src/main/webapp/app/src/layout/Header.js
+++ demeter-enterprise-manager/src/main/webapp/app/src/layout/Header.js
@@ -6,7 +6,7 @@ class Header extends Component{
   render(){
     return(
       <div className="Header">
-        <div><span className="Demeter">Demeter</span> Enterprise Server Manager ver. 2.1.0</div>
+        <div><span className="Demeter">Demeter</span> Enterprise Server Manager ver. 2.1.1</div>
       </div>
     );
   }
